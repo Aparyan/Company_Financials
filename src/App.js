5d7ea14e75fda18f5d1f385b236bb962f7fd78af
Aryan Prakash <ap2feb@gmail.com>
2024-03-01 21:34:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css'; // Import the CSS file
 
 function App() {
-  return (
+ const [symbol, setSymbol] = useState('');
+ const [displayedSymbol, setDisplayedSymbol] = useState('');
+ const [financials, setFinancials] = useState(null);
+ const [error, setError] = useState('');
+
+ const kpiItems = [
+    { label: 'Market Capitalization', key: 'marketCapitalization' },
+    { label: 'Enterprise Value', key: 'enterpriseValue' },
+    { label: 'Current Dividend Yield', key: 'currentDividendYieldTTM' },
+    { label: 'EPS Annual', key: 'epsAnnual' },
+    { label: 'Book Value Per Share (Annual)', key: 'bookValuePerShareAnnual' },
+    { label: 'Cash Flow Per Share (Annual)', key: 'cashFlowPerShareAnnual' },
+ ];
+
+ const fetchFinancials = async () => {
+    try {
+      const response = await axios.get(`https://finnhub.io/api/v1/stock/metric?symbol=${symbol}&metric=all&token=cn7fs9pr01qgjtj4jbs0cn7fs9pr01qgjtj4jbsg`);
+      if (Object.keys(response.data.metric).length === 0) {
+        setError('Company symbol not found or incorrect. Please try again!!');
+        setFinancials(null);
+      } else {
+        setFinancials(response.data);
+        setError('');
+        setDisplayedSymbol(symbol);
+      }
+    } catch (err) {
+      setError('An error occurred. Please try again.');
+      setFinancials(null);
+    }
+ };
+
+ return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Company's Basic Financials</h1>
+      <input
+        type="text"
+        placeholder="Enter company symbol"
+        value={symbol}
+        onChange={(e) => setSymbol(e.target.value)}
+      />
+      <button onClick={fetchFinancials}>Fetch Data</button>
+      {error && <p className="error">{error}</p>}
+      {financials && (
+        <div className="financials">
+          <h2>Financials of {displayedSymbol}</h2>
+          <div className="kpi-dashboard">
+            {kpiItems.map((item, index) => (
+              <div className="kpi-item" key={index}>
+                <p>{item.label}</p>
+                <p>{financials.metric[item.key]} $</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
-  );
+ );
 }
 
 export default App;
